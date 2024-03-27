d64a5ddd1b7ceec95a935018ad10d589de635972
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-03-27 13:55:20 +0700

    up sdk version to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mybottomnavigation"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mybottomnavigation"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
